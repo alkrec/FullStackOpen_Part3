778185fbf2d8b09643aae5889ec9169b5b60ac72
alkrec <118450684+alkrec@users.noreply.github.com>
2023-04-17 14:17:48 +0300

    removed front end build, added env port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(express.json())
-app.use(express.static('build'))
+// app.use(express.static('build'))
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :personOutput')) // from method to response-time, it is equilivant to 'tiny'
 
@@ -126,7 +126,7 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
